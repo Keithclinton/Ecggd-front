dd8fd07b1227c301db9a109b721cd3851d272a4a
Keithclinton <keithnyaburi@gmail.com>
2025-11-20 20:37:53 +0300

    Deployment

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2a6b4ca99203b674"; }
+/******/ 		__webpack_require__.h = function() { return "0d72155844019997"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: false
-  },
   async rewrites() {
     return [
       // Serve SVG favicon when browser requests /favicon.ico to avoid 404s in dev
